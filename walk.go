dd8e743b06f3b427aa7aca508da2023e186bda98
Nastya Kruglikova <nastya.kruglikova@gmail.com>
2016-04-11 22:41:54 +0300

    walk in folder and calculate hashes

---
+++ walk.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "os"
+    "path/filepath"
+    "strings"
+    "sync"
+    "io/ioutil"
+    "crypto/sha256"
+    "flag"
+    "encoding/hex"
+)
+
+const BlockSize = 64
+
+type item struct {
+    path string
+    parent *item
+    children []*item
+    hash  []byte
+}
+
+func print(item *item, offset int){
+    fmt.Println(strings.Repeat("   ", offset), "|")
+    fmt.Println(strings.Repeat("   ", offset), strings.Repeat("---", offset),
+        filepath.Base(item.path), hex.EncodeToString(item.hash))
+    for _, child := range item.children{
+        print(child, offset+1)
+    }
+}
+
+func getHash(item *item){
+    hasher := sha256.New()
+    s, err := ioutil.ReadFile(item.path)
+    if err != nil {
+        return
+    }
+    hasher.Write(s)
+    item.hash = hasher.Sum(nil)
+}
+
+func folderHash(item *item){
+    hasher := sha256.New()
+    for _, child := range item.children {
+        hasher.Write(child.hash)
+    }
+    item.hash = hasher.Sum(nil)
+}
+
+func main() {
+    dir := flag.String("dir", "testdir", "path to folder")
+    count := flag.Int("count", 4, "run program in concurent mode with n goroutines")
+    flag.Parse()
+    root, err := filepath.Abs(*dir)
+    if err != nil {
+        log.Fatal(err)
+    }
+    folders := make(map[string]*item)
+    saveItem := func(path string, info os.FileInfo, err error) error {
+        if err != nil {
+            return err
+        }
+        folders[path] = &item{
+            path: path,
+            children: make([]*item, 0),
+        }
+        return nil
+    }
+    if err = filepath.Walk(root, saveItem); err != nil {
+        log.Fatal(err)
+    }
+    var rootItem *item
+    for path, item := range folders {
+        parentItem, exists := folders[filepath.Dir(path)]
+        if exists {
+            item.parent = parentItem
+            parentItem.children = append(parentItem.children, item)
+        } else {
+            rootItem = item
+        }
+    }
+    ch := make (chan *item)
+    var wg sync.WaitGroup
+    wg.Add(*count)
+    for i:=0; i<(*count); i++ {
+        go func() {
+            defer wg.Done()
+            for {
+                if item, more := <- ch; more {
+                    getHash(item)
+                } else {
+                    return
+                }
+            }
+        }()
+    }
+    for _, item := range folders {
+        for _, child := range item.children {
+            ch <- child
+        }
+    }
+    for _, item := range folders {
+        folderHash(item)
+    }
+    close(ch)
+    wg.Wait()
+    print(rootItem, 0)
+}
